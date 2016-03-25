bdb30c534c4b455a272fb3f478e4d231d6a9f962
patsul12 <patsul12@gmail.com>
2016-03-25 16:46:58 -0700

    add seed data to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(username: "test_user", first_name: "Test", last_name: "User")
+
+companies = Company.create([{ name: "Google", address: "1234 Google Way", website: "www.google.com"},
+                           { name: "Apple", address: "1234 Apple Way", website: "www.apple.com" },
+                           { name: "Jaguer", address: "4567 Jaguar Way", website: "www.jaguar.com"},
+                           { name: "New Relic", address: "SW 6th Avenue", website: "www.newrelic.com" }])
+
+applications = JobApplication.create([{ company_id: 1, user_id: 1, date_submitted: "12/24/2015", status: "open"},
+                                     { company_id: 2, user_id: 1, date_submitted: "1/02/2016", status: "open" },
+                                     { company_id: 3, user_id: 1, date_submitted: "2/03/2016", status: "closed" },
+                                     { company_id: 4, user_id: 1, date_submitted: "3/04/2016", status: "closed" }])
