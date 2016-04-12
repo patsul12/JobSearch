277df46026c4d002044f401f741b6902a7c604c0
patsul12 <patsul12@gmail.com>
2016-04-12 09:27:54 -0700

    add methods to job posting model for counting shared job postings

--- app/models/job_posting.rb
+++ app/models/job_posting.rb
@@ -1,4 +1,12 @@
 class JobPosting < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :url
+
+  def position_count
+    JobPosting.where(position: self.position).count
+  end
+
+  def url_count
+    JobPosting.where(url: self.url).count
+  end
 end
