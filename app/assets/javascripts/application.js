7ff60264970914265efed972b1dc091f276e09c1
patsul12 <patsul12@gmail.com>
2016-04-21 12:56:53 -0700

    allow user to filter their contacts by any of the given fields

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -63,3 +63,14 @@ $.fn.stars = function() {
     });
 }
 
+$(function() {
+  $("#contacts-filter").on("keyup", function(e) {
+    var val = $(e.target).val();
+    var rows = $("#contacts-table tr");
+    val = $.trim(val).replace(/ +/g, " ").toLowerCase();
+    rows.show().filter(function() {
+      var text = $(this).text().replace(/\s+/g, " ").toLowerCase(); 
+      return !~text.indexOf(val); 
+    }).hide();
+  });
+});
